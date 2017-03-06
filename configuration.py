0ccc9948b7fda5877d893ce2e08e9c5aaf0e49ff
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2017-03-05 23:43:09 -0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,13 @@
+n_samples = 10000
+
+# Stage 1
+DETECTIONS = 'detections-20141206.csv'
+TRACKS = 'tracks.npz'
+
+# Stage 2
+TRACK_IDS = 'track_ids.pickle'
+TRACK_GENRES_BINARY = 'track_genres.pickle'
+
+# Stage 3
+SOURCE_GENRES = 'source_genres.csv'
+TRACK_GENRES = 'track_genres.csv'
